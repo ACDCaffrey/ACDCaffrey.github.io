4e6b4dd3a7fd9fd46f7ee30eba2a7a5f8d2fd854
Adam Caffrey <adamcaffrey@Adams-MacBook-Pro-2.local>
2020-10-29 08:32:33 -0700

    upload for review, not complete

--- javascript_apps/coincidence/coincidence_script.js
+++ javascript_apps/coincidence/coincidence_script.js
@@ -1,12 +1,3 @@
-
-var logic_pulse;
-logic_pulse = d3.csv("/lp.csv",
-  function(data){
-  return data;
-});
-
-console.log(logic_pulse[[PromiseResult]]);
-
 var svg = d3.select("#pulseplot")
             .classed("svg-container", true)
             .append("svg")
@@ -14,149 +5,150 @@ var svg = d3.select("#pulseplot")
             .attr("viewBox", "0 0 690 380")
             .classed("svg-content-responsive", true);
 
-var x = d3.scaleLinear().domain([0, 690]).range([0, 690]);
-var y = d3.scaleLinear().domain([0, 380]).range([0, 380]);
+d3.csv("./rfile2.csv",
+  function(data){
 
-svg.append("rect")
-   .attr("x", 30)
-   .attr("y", 100)
-   .attr("width", 80)
-   .attr("height", 40)
-   .attr("fill", "white")
-   .attr("stroke", "black");
+  var x = d3.scaleLinear().domain([0, 690]).range([0, 690]);
+  var y = d3.scaleLinear().domain([0, 380]).range([0, 380]);
 
-svg.append("rect")
-   .attr("x", 30)
-   .attr("y", 240)
-   .attr("width", 80)
-   .attr("height", 40)
-   .attr("fill", "white")
-   .attr("stroke", "black");
+  svg.append("rect")
+     .attr("x", 30)
+     .attr("y", 100)
+     .attr("width", 80)
+     .attr("height", 40)
+     .attr("fill", "white")
+     .attr("stroke", "black");
 
-svg.append("circle")
-   .attr("cx", 70)
-   .attr("cy", 190)
-   .attr("r", 5)
+  svg.append("rect")
+     .attr("x", 30)
+     .attr("y", 240)
+     .attr("width", 80)
+     .attr("height", 40)
+     .attr("fill", "white")
+     .attr("stroke", "black");
 
-svg.append("text")
-   .attr("x", 2)
-   .attr("y", 195)
-   .text("Source");
+  svg.append("circle")
+     .attr("cx", 70)
+     .attr("cy", 190)
+     .attr("r", 5)
 
-svg.append("line")
-   .attr("x1", 110)
-   .attr("x2", 625)
-   .attr("y1", 120)
-   .attr("y2", 120)
-   .attr("stroke", "black");
+  svg.append("text")
+     .attr("x", 2)
+     .attr("y", 195)
+     .text("Source");
 
-svg.append("line")
-   .attr("x1", 110)
-   .attr("x2", 625)
-   .attr("y1", 260)
-   .attr("y2", 260)
-   .attr("stroke", "black");
+  svg.append("line")
+     .attr("x1", 110)
+     .attr("x2", 625)
+     .attr("y1", 120)
+     .attr("y2", 120)
+     .attr("stroke", "black");
 
- svg.append("rect")
-    .attr("x", 190)
+  svg.append("line")
+     .attr("x1", 110)
+     .attr("x2", 625)
+     .attr("y1", 260)
+     .attr("y2", 260)
+     .attr("stroke", "black");
+
+   svg.append("rect")
+      .attr("x", 190)
+      .attr("y", 100)
+      .attr("width", 70)
+      .attr("height", 40)
+      .attr("fill", "white")
+      .attr("stroke", "black");
+
+   svg.append("rect")
+      .attr("x", 190)
+      .attr("y", 240)
+      .attr("width", 70)
+      .attr("height", 40)
+      .attr("fill", "white")
+      .attr("stroke", "black");
+
+  svg.append("rect")
+    .attr("x", 350)
     .attr("y", 100)
     .attr("width", 70)
     .attr("height", 40)
     .attr("fill", "white")
     .attr("stroke", "black");
 
- svg.append("rect")
-    .attr("x", 190)
+  svg.append("rect")
+    .attr("x", 350)
     .attr("y", 240)
     .attr("width", 70)
     .attr("height", 40)
     .attr("fill", "white")
     .attr("stroke", "black");
 
-svg.append("rect")
-  .attr("x", 350)
-  .attr("y", 100)
-  .attr("width", 70)
-  .attr("height", 40)
-  .attr("fill", "white")
-  .attr("stroke", "black");
+  svg.append("line")
+     .attr("x1", 625)
+     .attr("x2", 625)
+     .attr("y1", 260)
+     .attr("y2", 120)
+     .attr("stroke", "black");
 
-svg.append("rect")
-  .attr("x", 350)
-  .attr("y", 240)
-  .attr("width", 70)
-  .attr("height", 40)
-  .attr("fill", "white")
-  .attr("stroke", "black");
+  svg.append("rect")
+     .attr("x", 590)
+     .attr("y", 170)
+     .attr("width", 70)
+     .attr("height", 40)
+     .attr("fill", "white")
+     .attr("stroke", "black");
 
-svg.append("line")
-   .attr("x1", 625)
-   .attr("x2", 625)
-   .attr("y1", 260)
-   .attr("y2", 120)
-   .attr("stroke", "black");
+  svg.append("rect")
+     .attr("x", 470)
+     .attr("y", 100)
+     .attr("width", 70)
+     .attr("height", 40)
+     .attr("fill", "white")
+     .attr("stroke", "black");
 
-svg.append("rect")
-   .attr("x", 590)
-   .attr("y", 170)
-   .attr("width", 70)
-   .attr("height", 40)
-   .attr("fill", "white")
-   .attr("stroke", "black");
+  svg.append("text")
+     .attr("x", 33)
+     .attr("y", 126)
+     .text("Detector 1");
 
-svg.append("rect")
-   .attr("x", 470)
-   .attr("y", 100)
-   .attr("width", 70)
-   .attr("height", 40)
-   .attr("fill", "white")
-   .attr("stroke", "black");
+  svg.append("text")
+     .attr("x", 209)
+     .attr("y", 126)
+     .text("TFA");
 
-svg.append("text")
-   .attr("x", 33)
-   .attr("y", 126)
-   .text("Detector 1");
+  svg.append("text")
+    .attr("x", 369)
+    .attr("y", 126)
+    .text("CFD");
 
-svg.append("text")
-   .attr("x", 209)
-   .attr("y", 126)
-   .text("TFA");
+  svg.append("text")
+     .attr("x", 485)
+     .attr("y", 126)
+     .text("Delay");
 
-svg.append("text")
-  .attr("x", 369)
-  .attr("y", 126)
-  .text("CFD");
+  svg.append("text")
+     .attr("x", 610)
+     .attr("y", 197)
+     .text("TAC");
 
-svg.append("text")
-   .attr("x", 485)
-   .attr("y", 126)
-   .text("Delay");
+  svg.append("text")
+     .attr("x", 33)
+     .attr("y", 265)
+     .text("Detector 2");
 
-svg.append("text")
-   .attr("x", 610)
-   .attr("y", 197)
-   .text("TAC");
+  svg.append("text")
+     .attr("x", 209)
+     .attr("y", 265)
+     .text("TFA");
 
-svg.append("text")
-  .attr("x", 33)
-  .attr("y", 265)
-  .text("Detector 2");
+  svg.append("text")
+     .attr("x", 369)
+     .attr("y", 265)
+     .text("CFD");
 
-svg.append("text")
-   .attr("x", 209)
-   .attr("y", 265)
-   .text("TFA");
+  function gogopowerrangers(){
 
-svg.append("text")
-   .attr("x", 369)
-   .attr("y", 265)
-   .text("CFD");
-
-function gogopowerrangers(){
-  repeat();
-}
-
-function repeat(){
+    svg.selectAll("path").remove();
 
     var ldata1 = [
       {x: 70, y: 185},
@@ -170,10 +162,7 @@ function repeat(){
 
     var line = d3.line()
                  .x(function(ldata){return x(ldata.x)})
-                 .y(function(ldata){return y(ldata.y)})
-                 .curve(d3.curveNatural)
-
-    svg.selectAll("path").remove();
+                 .y(function(ldata){return y(ldata.y)});
 
     var path1 = svg.append("path")
                   .attr("d", line(ldata1))
@@ -188,7 +177,7 @@ function repeat(){
     var totalLength = path1.node().getTotalLength();
 
     path1
-      .attr("stroke-dasharray", totalLength + " " + 10)
+      .attr("stroke-dasharray", totalLength + " " + 50)
       .attr("stroke-dashoffset", totalLength)
       .transition()
       .duration(1000)
@@ -197,112 +186,270 @@ function repeat(){
       .attr("stroke", 'black');
 
     path2
-      .attr("stroke-dasharray", totalLength + " " + 10)
+      .attr("stroke-dasharray", totalLength + " " + 50)
       .attr("stroke-dashoffset", totalLength)
       .transition()
       .duration(1000)
       .ease(d3.easeLinear)
       .attr("stroke-dashoffset", 0)
+      .attr("stroke", 'black');
+
+    var dataFilter1 = data.map(function(d){return {x: d.x, y: d["pp"]} })
+    var dataFilter2 = data.map(function(d){return {x: d.x, y: d["pp"]} })
+
+    var dataFilter1b = data.map(function(d){return {x: d.x, y: d["pp"]} })
+    var dataFilter2b = data.map(function(d){return {x: d.x, y: d["pp"]} })
+
+    var dataFilter3 = data.map(function(d){return {x: d.x, y: d["tp"]} })
+    var dataFilter4 = data.map(function(d){return {x: d.x, y: d["tp"]} })
+
+    var dataFilter3b = data.map(function(d){return {x: d.x, y: d["tp"]} })
+    var dataFilter4b = data.map(function(d){return {x: d.x, y: d["tp"]} })
+
+    var dataFilter5 = data.map(function(d){return {x: d.x, y: d["lp"]} })
+    var dataFilter6 = data.map(function(d){return {x: d.x, y: d["lp"]} })
+
+    var dataFilter5b = data.map(function(d){return {x: d.x, y: d["lp"]} })
+    var dataFilter6b = data.map(function(d){return {x: d.x, y: d["lp"]} })
+
+    var dataFilter5b2 = data.map(function(d){return {x: d.x, y: d["lp"]} })
+    var dataFilter6b2 = data.map(function(d){return {x: d.x, y: d["lp"]} })
+
+    for(var i = 0; i < 50; i++){
+      dataFilter1[i].y = parseFloat(dataFilter1[i].y) + 310;
+      dataFilter2[i].y = parseFloat(dataFilter2[i].y) + 310;
+
+      dataFilter1[i].x = parseFloat(dataFilter1[i].x) + 45;
+      dataFilter2[i].x = parseFloat(dataFilter2[i].x) + 200;
+
+      // 3000 for 155. 122 = 2361, 123 = 2380, 245 = 4742
+
+      dataFilter3[i].y = parseFloat(dataFilter3[i].y) + 310;
+      dataFilter4[i].y = parseFloat(dataFilter4[i].y) + 310;
+
+      dataFilter3[i].x = parseFloat(dataFilter3[i].x) + 200;
+      dataFilter4[i].x = parseFloat(dataFilter4[i].x) + 355;
+
+      dataFilter5[i].y = parseFloat(dataFilter5[i].y) + 310;
+      dataFilter6[i].y = parseFloat(dataFilter6[i].y) + 310;
+
+      dataFilter5[i].x = parseFloat(dataFilter5[i].x) + 355;
+      dataFilter6[i].x = parseFloat(dataFilter6[i].x) + 600;
+
+      // inverted pulses start here
+
+      dataFilter1b[i].y = 380 - parseFloat(dataFilter1b[i].y) - 310;
+      dataFilter2b[i].y = 380 - parseFloat(dataFilter2b[i].y) - 310;
+
+      dataFilter1b[i].x = parseFloat(dataFilter1b[i].x) + 45;
+      dataFilter2b[i].x = parseFloat(dataFilter2b[i].x) + 200;
+
+      dataFilter3b[i].y = 380 - parseFloat(dataFilter3b[i].y) - 310;
+      dataFilter4b[i].y = 380 - parseFloat(dataFilter4b[i].y) - 310;
+
+      dataFilter3b[i].x = parseFloat(dataFilter3b[i].x) + 200;
+      dataFilter4b[i].x = parseFloat(dataFilter4b[i].x) + 355;
+
+      dataFilter5b[i].y = 380 - parseFloat(dataFilter5b[i].y) - 310;
+      dataFilter6b[i].y = 380 - parseFloat(dataFilter6b[i].y) - 310;
+      dataFilter6b2[i].y = 380 - parseFloat(dataFilter6b2[i].y) - 310;
+
+      dataFilter5b[i].x = parseFloat(dataFilter5b[i].x) + 355;
+      dataFilter6b[i].x = parseFloat(dataFilter6b[i].x) + 477;
+      dataFilter6b2[i].x = parseFloat(dataFilter6b2[i].x) + 600;
+
+    }
+
+    // console.log(dataFilter);
+
+    var pathbottom = svg.append("path")
+                        .attr("d", line(dataFilter1))
+                        .attr("stroke-width", "2")
+                        .attr("fill", "none");
+
+    var pathbottom2 = svg.append("path")
+                         .attr("d", line(dataFilter2))
+                         .attr("stroke-width", "2")
+                         .attr("fill", "none");
+
+    var pathbottom3 = svg.append("path")
+                         .attr("d", line(dataFilter3))
+                         .attr("stroke-width", "2")
+                         .attr("fill", "none");
+
+    var pathbottom4 = svg.append("path")
+                         .attr("d", line(dataFilter4))
+                         .attr("stroke-width", "2")
+                         .attr("fill", "none");
+
+    var pathbottom5 = svg.append("path")
+                         .attr("d", line(dataFilter5))
+                         .attr("stroke-width", "2")
+                         .attr("fill", "none");
+
+    var pathtop = svg.append("path")
+                     .attr("d", line(dataFilter1b))
+                     .attr("stroke-width", "2")
+                     .attr("fill", "none");
+
+    var pathtop2 = svg.append("path")
+                      .attr("d", line(dataFilter2b))
+                      .attr("stroke-width", "2")
+                      .attr("fill", "none");
+
+    var pathtop3 = svg.append("path")
+                      .attr("d", line(dataFilter3b))
+                      .attr("stroke-width", "2")
+                      .attr("fill", "none");
+
+    var pathtop4 = svg.append("path")
+                      .attr("d", line(dataFilter4b))
+                      .attr("stroke-width", "2")
+                      .attr("fill", "none");
+
+    var pathtop5 = svg.append("path")
+                      .attr("d", line(dataFilter5b))
+                      .attr("stroke-width", "2")
+                      .attr("fill", "none");
+
+    var pathtop6 = svg.append("path")
+                      .attr("d", line(dataFilter6b))
+                      .attr("stroke-width", "2")
+                      .attr("fill", "none");
+
+    var pathtop7 = svg.append("path")
+                      .attr("d", line(dataFilter6b))
+                      .attr("stroke-width", "2")
+                      .attr("fill", "none");
+
+
+    pathbottom
+      .transition()
+      .attr("d", line(dataFilter2))
+      .delay(1400)
       .attr("stroke", 'black')
-      .on("end", repeat);
+      .duration(3000)
+      .ease(d3.easeLinear)
+      .on("end", function() {
+        d3.select(this).attr("opacity", 0);
+      });
 
-}
+    pathtop
+      .transition()
+      .attr("d", line(dataFilter2b))
+      .delay(1000)
+      .attr("stroke", 'black')
+      .duration(3000)
+      .ease(d3.easeLinear)
+      .on("end", function() {
+        d3.select(this).attr("opacity", 0);
+      });
 
-// Read the data
-// d3.csv("./lp.csv",
-//
-//   function(data) {
-//
-//     var delay = 150;
-//
-//     // Add X axis
-//     var x = d3.scaleLinear()
-//               .domain([0, 1260]) // axis limits
-//               .range([ 0, 600 ]);
-//
-//     svg.append("g")
-//       .attr("transform", "translate(70," + 310 + ")") // x-axis shift
-//       .call(d3.axisBottom(x));
-//
-//     // text label for the x axis
-//     svg.append("text")
-//        .attr("transform",
-//              "translate(" + 390 + " ," + 360 + ")")
-//        .style("text-anchor", "middle")
-//        .text("Time (ns)");
-//
-//     // Add Y axis
-//     var y = d3.scaleLinear()
-//               .domain([0, 100]) // axis limits
-//               .range([300, 0]);
-//
-//     yAxis = svg.append("g")
-//        .attr("transform", "translate(" + 70 + ",10)") // y-axis shift
-//        .call(d3.axisLeft(y));
-//
-//     // text label for the y axis
-//     svg.append("text")
-//         .attr("transform", "rotate(-90)") // vertical text
-//         .attr("y", 10)
-//         .attr("x", -150)
-//         .attr("dy", "1em")
-//         .style("text-anchor", "middle")
-//         .text("Normalised Amplitude");
-//
-//     // Initialize line with group a
-//     var logic_line1 = svg
-//       .append('g')
-//       .append("path")
-//         .datum(data)
-//         .attr("d", d3.line()
-//           .x(function(d) { return x(d.x) + 70})
-//           .y(function(d) { return y(d.y) + 10})
-//         )
-//         .attr("stroke", "steelblue")
-//         .style("stroke-width", 1.5)
-//         .style("fill", "none")
-//
-//   var logic_line2 = svg
-//     .append('g')
-//     .append("path")
-//       .datum(data)
-//       .attr("d", d3.line()
-//         .x(function(d) { return x(d.x) + 70})
-//         .y(function(d) { return y(d.y) - 50})
-//       )
-//       .attr("stroke", "steelblue")
-//       .style("stroke-width", 1.5)
-//       .style("fill", "none")
-//
-//     logic_line1
-//       .attr('cx', 40)
+    pathbottom2
+      .transition()
+      .attr("d", line(dataFilter3))
+      .attr("stroke", 'black')
+      .delay(4400)
+      .duration(300)
+      .on("end", function() {
+        d3.select(this).attr("opacity", 0);
+      });
 
-    // for(var step = 0; step < 500; step++){
-    //
-    //     logic_line1
-    //       .datum(data)
-    //       .transition()
-    //       .ease(d3.easeLinear)
-    //       .duration(5000)
-    //       .attr("d", d3.line()
-    //         .x(function(d) { return x(d.x) + step })
-    //         .y(function(d) { return y(d.y)})
-    //       )
-    //       .attr("stroke", "steelblue")
-    //
-    //     logic_line2
-    //       .datum(data)
-    //       .transition()
-    //       .ease(d3.easeLinear)
-    //       .duration(5000)
-    //       .attr("d", d3.line()
-    //         .x(function(d) { return x(d.x) + step })
-    //         .y(function(d) { return y(d.y) - 50})
-    //       )
-    //       .attr("stroke", "steelblue")
-    //
-    // }
+    pathtop2
+      .transition()
+      .attr("d", line(dataFilter3b))
+      .attr("stroke", 'black')
+      .delay(4000)
+      .duration(300)
+      .on("end", function() {
+        d3.select(this).attr("opacity", 0);
+      });
+
+    pathbottom3
+      .transition()
+      .attr("d", line(dataFilter4))
+      .delay(4700)
+      .attr("stroke", 'black')
+      .duration(3000)
+      .ease(d3.easeLinear)
+      .on("end", function() {
+        d3.select(this).attr("opacity", 0);
+      });
+
+    pathtop3
+      .transition()
+      .attr("d", line(dataFilter4b))
+      .delay(4300)
+      .attr("stroke", 'black')
+      .duration(3000)
+      .ease(d3.easeLinear)
+      .on("end", function() {
+        d3.select(this).attr("opacity", 0);
+      });
+
+    pathbottom4
+      .transition()
+      .attr("d", line(dataFilter5))
+      .attr("stroke", 'black')
+      .delay(7700)
+      .duration(300)
+      .on("end", function() {
+        d3.select(this).attr("opacity", 0);
+      });
 
 
-//   }
-// )
+    pathtop4
+      .transition()
+      .attr("d", line(dataFilter5b))
+      .attr("stroke", 'black')
+      .delay(7300)
+      .duration(300)
+      .on("end", function() {
+        d3.select(this).attr("opacity", 0);
+      });
+
+    pathbottom5
+      .transition()
+      .attr("d", line(dataFilter6))
+      .delay(8000)
+      .attr("stroke", 'black')
+      .duration(4742)
+      .ease(d3.easeLinear);
+
+    pathtop5
+      .transition()
+      .attr("d", line(dataFilter6b))
+      .delay(7600)
+      .attr("stroke", 'black')
+      .duration(2361)
+      .ease(d3.easeLinear)
+      .on("end", function() {
+        d3.select(this).attr("opacity", 0);
+      });
+
+
+    pathtop6 // delay
+      .transition()
+      .attr("d", line(dataFilter6b))
+      .attr("stroke", 'black')
+      .delay(9961)
+      .duration(400)
+      .on("end", function() {
+        d3.select(this).attr("opacity", 0);
+      });
+
+    pathtop7
+      .transition()
+      .attr("d", line(dataFilter6b2))
+      .delay(10361)
+      .attr("stroke", 'black')
+      .duration(2380)
+      .ease(d3.easeLinear);
+
+  }
+
+  d3.select("#powerranger").on("click", function(d) {
+    gogopowerrangers();
+  })
+
+});
--- javascript_apps/comptonapp/script.js
+++ javascript_apps/comptonapp/script.js
@@ -23,14 +23,15 @@ var x = d3.scaleLinear()
           .range([ 0, 500 ]);
 
 svg.append("g")
-   .attr("transform", "translate(120," + 350 + ")") // x-axis shift
+   .attr("transform", "translate(70," + 350 + ")") // x-axis shift
    .call(d3.axisBottom(x));
 
 // text label for the x axis
 svg.append("text")
    .attr("transform",
-         "translate(" + 400 + " ," + 395 + ")")
+         "translate(" + 330 + " ," + 395 + ")")
    .style("text-anchor", "middle")
+   .attr("font-family", "sans-serif")
    .text("Angle (degrees)");
 
 // Add Y axis
@@ -38,17 +39,18 @@ var y = d3.scaleLinear()
           .domain([0, 1500]) // axis limits
           .range([330, 0 ]);
 
-svg.append("g")
-  .attr("transform", "translate(" + 120 + ",20)") // y-axis shift
-  .call(d3.axisLeft(y));
+yAxis = svg.append("g")
+           .attr("transform", "translate(" + 70 + ",20)") // y-axis shift
+           .call(d3.axisLeft(y));
 
 // text label for the y axis
 svg.append("text")
     .attr("transform", "rotate(-90)") // vertical text
-    .attr("y", 50)
-    .attr("x", -150)
+    .attr("y", 5)
+    .attr("x", -160)
     .attr("dy", "1em")
     .style("text-anchor", "middle")
+    .attr("font-family", "sans-serif")
     .text("Energy (keV)");
 
 var line1 = svg
@@ -56,7 +58,7 @@ var line1 = svg
   .append("path")
     .datum(data_orig)
     .attr("d", d3.line() // .curve(d3.curveStepAfter)
-      .x(function(d) {return x(d.x) + 120;})
+      .x(function(d) {return x(d.x) + 70;})
       .y(function(d) {return y(d.y) + 20;})
     )
     .attr("stroke", "steelblue")
@@ -68,7 +70,7 @@ var line2 = svg
   .append("path")
     .datum(data_orig2)
     .attr("d", d3.line() // .curve(d3.curveStepAfter)
-      .x(function(d) {return x(d.x) + 120;})
+      .x(function(d) {return x(d.x) + 70;})
       .y(function(d) {return y(d.y) + 20;})
     )
     .attr("stroke", "orange")
@@ -80,7 +82,7 @@ var line2 = svg
       .range(["orange", "steelblue"]);
 
   var size = 15
-  
+
   svg.selectAll("mylines")
     .data(keys)
     .enter()
@@ -98,38 +100,93 @@ var line2 = svg
     .append("text")
       .attr("x", 450 + size*1.2)
       .attr("y", function(d,i){ return 20 + i*(size+5) + (size/2)}) // 100 is where the first dot appears. 25 is the distance between dots
-      //.style("fill", function(d){ return color(d)})
       .text(function(d){ return d})
+      .attr("font-family", "sans-serif")
       .attr("text-anchor", "left")
       .style("alignment-baseline", "middle");
+
+var svg2 = d3.select("#comptonapptooltip")
+             .classed("svg-container", true)
+             .append("svg")
+             .attr("preserveAspectRatio", "xMinYMin meet")
+             .attr("viewBox", "0 0 250 40")
+             .classed("svg-content-responsive", true);
+
+var tooltip = svg2.append("g")
+                  .style("display", "none");
+
+tooltip.append("rect")
+       .attr("class", "tooltipbox")
+       .attr("fill", "#eee")
+       .style("opacity", 1)
+       .attr("width", 85)
+       .attr("height", 50)
+       .attr("x", 0)
+       .attr("y", 0);
+
+tooltip.append("text")
+       .attr("class", "tooltiptext1")
+       .attr("x", 2)
+       .attr("y", 10);
+
+tooltip.append("text")
+       .attr("class", "tooltiptext2")
+       .attr("x", 2)
+       .attr("y", 22);
+
+tooltip.append("text")
+       .attr("class", "tooltiptext3")
+       .attr("x", 2)
+       .attr("y", 34);
+
+svg.append("rect")
+    .attr("class", "overlay")
+    .attr("width", 500)
+    .attr("height", 330)
+    .attr("x", 70)
+    .attr("y", 20)
+    .on("mouseover", function() { tooltip.style("display", null);})
+    .on("mouseout",  function() { tooltip.style("display", "none");})
+    .on("mousemove", mousemove);
+
+
+  function mousemove() {
+      var x0 = x.invert(d3.mouse(this)[0]),
+           i = Math.round(x0-1-43+19),
+          d0 = Math.round(data_orig[i].y*10)/10,
+          d1 = Math.round(data_orig2[i].y*10)/10;
+      tooltip.attr("x", 70);
+      tooltip.attr("y", 20);
+      tooltip.select(".tooltiptext1").text("Angle: " + i + " Degrees");
+      tooltip.select(".tooltiptext2").text("Outgoing Electron Energy: " + d0 + " keV");
+      tooltip.select(".tooltiptext3").text("Outgoing Gamma-ray Energy: " + d1 + " keV");
+  }
 
 function updatecomptonplot(event){
   var energy = document.getElementById("energyinput").value;
 
-  var data = [];
-  var data2 = [];
-  data[0] = {x:0, y:0};
-  data2[0] = {x:0, y:0};
   for(var i = 0; i < 181; i++){
-    data[i] = {x: i, y: energy/(1 + (energy/511)*(1 - Math.cos(i*Math.PI/180)))}
-    data2[i] = {x: i, y: energy - energy/(1 + (energy/511)*(1 - Math.cos(i*Math.PI/180)))}
+    data_orig[i] = {x: i, y: energy/(1 + (energy/511)*(1 - Math.cos(i*Math.PI/180)))}
+    data_orig2[i] = {x: i, y: energy - energy/(1 + (energy/511)*(1 - Math.cos(i*Math.PI/180)))}
   }
 
+  y.domain([0,energy*1.2])
+    yAxis.transition().duration(1000).call(d3.axisLeft(y));
+
   line1
-    .datum(data)
+    .datum(data_orig)
     .transition()
     .duration(0)
     .attr("d", d3.line() //.curve(d3.curveStepAfter)
-      .x(function(d) {return x(d.x) + 120;})
-      .y(function(d) {return y(d.y) + 20;})
-    );
+      .x(function(d) {return x(d.x) + 70;})
+      .y(function(d) {return y(d.y) + 20;}));
 
   line2
-    .datum(data2)
+    .datum(data_orig2)
     .transition()
     .duration(0)
     .attr("d", d3.line() // .curve(d3.curveStepAfter)
-      .x(function(d) {return x(d.x) + 120;})
+      .x(function(d) {return x(d.x) + 70;})
       .y(function(d) {return y(d.y) + 20;})
     );
 }
